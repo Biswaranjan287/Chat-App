58523a4b3ae39baf77defd85874f82e2adaab1fc
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2025-11-07 19:38:57 +0530

    Add initial components and assets for chat application

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
+    return (
+        <div>
+            <div className='pb-5'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
