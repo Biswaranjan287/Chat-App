387ee109f0fd4cd2b939cafcada4d6950ef90d7d
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2025-12-04 11:01:21 +0530

    Add AuthContext for user authentication and socket connection management

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,64 @@
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { io } from 'socket.io-client'
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL
+axios.defaults.baseURL = backendUrl
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({ Children }) => {
+
+    const [token, setToken] = useState(localStorage.getItem("token"))
+    const [authUser, setAuthUser] = useState(null)
+    const [onlineUser, setOnlineUser] = useState([])
+    const [socket, setSocket] = useState(null)
+
+    const checkAuth = async () => {
+        try {
+            const { data } = await axios.get("/api/auth/check")
+            if (data.success) {
+                setAuthUser(data.user)
+                connectSocket(data.user)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const connectSocket = (userData) => {
+        if (!userData || socket?.connected) return
+        const newSocket = io(backendUrl, {
+            query: {
+                userId: userData._id,
+            }
+        })
+        newSocket.connect()
+        setSocket(newSocket)
+
+        newSocket.on("getOnlineUsers", (userIds)=>{
+            setOnlineUser(userIds)
+        })
+    }
+
+    useEffect(() => {
+        if (token) {
+            axios.defaults.headers.common["token"] = token
+        }
+        checkAuth()
+    }, [])
+
+    const value = {
+        axios,
+        authUser,
+        onlineUser,
+        socket
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {Children}
+        </AuthContext.Provider>
+    )
+}
